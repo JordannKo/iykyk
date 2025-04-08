bc9b3a1ef9036ca2bebce1afb67efe400f8b7cbc
JordannKo <jordannn0023@gmail.com>
2025-04-08 18:50:57 +0900

    create project

---
+++ iykyk/iykyk/iykykApp.swift
@@ -0,0 +1,17 @@
+//
+//  iykykApp.swift
+//  iykyk
+//
+//  Created by JordannKo on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct iykykApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
